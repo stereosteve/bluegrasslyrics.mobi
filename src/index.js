df49be6e6d0bb40607ef3ab7241144b3918f30f2
Steve Perkins <steve@octopart.com>
2019-09-02 11:29:07 -0400

    song list detail

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Link, Router } from '@reach/router'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import * as serviceWorker from './serviceWorker'
+import songs from './songs.json'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function SongList({ children }) {
+  return (
+    <div>
+      <div className="SongList">
+        {songs.map(song => {
+          return (
+            <div className="item">
+              <Link to={`/song/${song.title}`}>{song.title}</Link>
+            </div>
+          )
+        })}
+      </div>
+      {children}
+    </div>
+  )
+}
+
+function SongDetail({ id }) {
+  const song = songs.find(s => s.title === id)
+  return (
+    <div className="SongDetail">
+      <div className="inner">
+        <Link to="/">close</Link>
+        <h1>{id}</h1>
+        <div dangerouslySetInnerHTML={{ __html: song.content }} />
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <div>
+    <Router primary={false}>
+      <SongList path="/">
+        <SongDetail path="song/:id" />
+      </SongList>
+    </Router>
+  </div>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
