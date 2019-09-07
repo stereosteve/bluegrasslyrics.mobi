d9fd09717bf596159ba455bd26f86d968dd05fa1
Steve Perkins <steve@octopart.com>
2019-09-07 05:31:39 -0400

    Some tailwind css bs
    * Scroll on phone is all jacked up.  Should use a list component.
    * Should have placeholder when no stars / no songs matched
    * Scrolling on song detail view???

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { Link, Router } from '@reach/router'
 import React, { useState } from 'react'
 import allSongs from './songs.json'
 
+const cx = {}
+cx.button = ``
+cx.button_on = ``
+
 // on boot load existing stars from localstorage
 const initialStars = JSON.parse(localStorage.getItem('stars') || '{}')
 
@@ -18,46 +22,92 @@ function SongList({ children, stars }) {
     songs = songs.filter(s => stars[s.slug])
   }
 
+  const toggleStarFilter = () => {
+    setStarFilter(!starFilter)
+    setQ('')
+  }
+
+  const updateQ = e => {
+    setStarFilter(false)
+    setQ(e.target.value)
+  }
+
   return (
     <div>
-      <div className="search-bar">
+      <div className="flex p-2 border-b-2 border-red-900 bg-red-800 fixed w-full top-0">
         <input
-          type="search"
+          className="flex-grow border border-gray-500 rounded-none p-3 focus:outline-none focus:bg-yellow-100"
+          type="text"
           placeholder="Search"
           value={q}
-          onChange={e => setQ(e.target.value)}
+          onChange={updateQ}
         />
-        <button onClick={() => setStarFilter(!starFilter)}>
-          {starFilter ? '****' : '*'}
+        <button
+          onClick={toggleStarFilter}
+          className={`flex p-3 border border-l-0 border-gray-500 focus:outline-none ${
+            starFilter ? 'bg-red-500' : 'bg-green-100'
+          }`}
+        >
+          <Star on={starFilter} />
         </button>
       </div>
+      <div className="pb-12">&nbsp;</div>
       <div className="SongList">
-        {songs.map(song => {
-          return (
-            <div className="item" key={song.slug}>
-              <Link to={`/song/${song.slug}`}>{song.title}</Link>
-              {stars[song.slug] ? '*' : ''}
-            </div>
-          )
-        })}
+        {songs.map(song => (
+          <SongListItem
+            song={song}
+            isStarred={stars[song.slug]}
+            key={song.slug}
+          />
+        ))}
       </div>
       {children}
     </div>
   )
 }
 
+const Star = ({ on = false }) => <span>{on ? '🌟' : '⭐'}</span>
+
+const SongListItem = ({ song, isStarred }) => {
+  return (
+    <div className="p-3 border-b border-gray-400 font-bold">
+      <Link to={`/song/${song.slug}`}>{song.title}</Link>
+      {isStarred && (
+        <span className="float-right">
+          <Star />
+        </span>
+      )}
+    </div>
+  )
+}
+
 function SongDetail({ slug, stars, toggleStar }) {
   const song = allSongs.find(s => s.slug === slug)
+  const isStarred = stars[song.slug]
 
   return (
-    <div className="SongDetail">
-      <div className="inner">
-        <Link to="/">close</Link>
-        <button onClick={() => toggleStar(song)}>
-          {stars[song.slug] ? 'Unstar' : 'Star'}
+    <div className="fixed top-0 w-full h-full bg-white overflow-auto z-10">
+      <div className="flex p-2 bg-gray-200">
+        <Link to="/" className="flex p-2 w-32 text-center bg-gray-100">
+          ❌ Close
+        </Link>
+        <button
+          onClick={() => toggleStar(song)}
+          className={`flex ml-2 p-2 w-32 focus:outline-none ${
+            isStarred ? 'bg-green-300' : 'bg-gray-100'
+          }`}
+        >
+          <Star on={isStarred} />
+          {isStarred ? 'Unstar' : 'Star'}
         </button>
-        <h1>{song.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: song.content }} />
+      </div>
+
+      <div className="p-2">
+        <h1 className="text-xl font-bold mb-4">{song.title}</h1>
+        <div
+          className="content"
+          dangerouslySetInnerHTML={{ __html: song.content }}
+        />
       </div>
     </div>
   )
@@ -73,7 +123,7 @@ export default function App() {
   }
 
   return (
-    <div>
+    <div className="antialiased font-serif">
       <Router primary={false}>
         <SongList path="/" stars={stars}>
           <SongDetail path="song/:slug" stars={stars} toggleStar={toggleStar} />
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import './tailwind.min.css'
 import * as serviceWorker from './serviceWorker'
 import App from './App'
 
